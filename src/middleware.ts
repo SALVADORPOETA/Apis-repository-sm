f3c6748ec44cb622635596139c2f2b5905753c40
Salvador Poeta <dsmlhidroponia@gmail.com>
2025-10-15 10:43:51 -0600

    test1

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,27 +1,27 @@
-// src/middleware.ts
-import { NextResponse } from 'next/server'
-import type { NextRequest } from 'next/server'
+// // src/middleware.ts
+// import { NextResponse } from 'next/server'
+// import type { NextRequest } from 'next/server'
 
-export function middleware(request: NextRequest) {
-  const { pathname } = request.nextUrl
-  const cookie = request.cookies.get('admin-access') // 1. Si intenta acceder a cualquier ruta /admin/*
+// export function middleware(request: NextRequest) {
+//   const { pathname } = request.nextUrl
+//   const cookie = request.cookies.get('admin-access') // 1. Si intenta acceder a cualquier ruta /admin/*
 
-  if (pathname.startsWith('/admin')) {
-    // 2. Si no tiene la cookie o la cookie no es válida
-    if (!cookie || cookie.value !== 'true') {
-      // Redirige a /admin, pero solo si no está ya en la ruta base /admin
-      // Esto ayuda a evitar un bucle de redireccionamiento si tu página /admin
-      // es donde se muestra el AuthModal.
-      if (pathname !== '/admin') {
-        return NextResponse.redirect(new URL('/admin', request.url))
-      }
-    }
-  }
+//   if (pathname.startsWith('/admin')) {
+//     // 2. Si no tiene la cookie o la cookie no es válida
+//     if (!cookie || cookie.value !== 'true') {
+//       // Redirige a /admin, pero solo si no está ya en la ruta base /admin
+//       // Esto ayuda a evitar un bucle de redireccionamiento si tu página /admin
+//       // es donde se muestra el AuthModal.
+//       if (pathname !== '/admin') {
+//         return NextResponse.redirect(new URL('/admin', request.url))
+//       }
+//     }
+//   }
 
-  return NextResponse.next()
-}
+//   return NextResponse.next()
+// }
 
-// Indica a Next.js que se aplique a todas las rutas bajo /admin/*
-export const config = {
-  matcher: ['/admin/:path*'],
-}
+// // Indica a Next.js que se aplique a todas las rutas bajo /admin/*
+// export const config = {
+//   matcher: ['/admin/:path*'],
+// }
